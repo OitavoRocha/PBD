b70f7a9dd7058d2f614a75ee797995e98464f7d6
Otávio Rocha <otaviosrocha30@gmail.com>
2024-02-24 20:27:45 -0300

    Create testes.sql

---
+++ testes.sql
@@ -0,0 +1,13 @@
+INSERT INTO Usuario VALUES(1, '123456789', 5.0, 'Joao');
+
+INSERT INTO Usuario VALUES(2, '00000000', 3.0, 'Otavio');
+
+INSERT INTO Usuario VALUES(3, '987654321', 4.2, 'Miguel');
+
+SELECT * FROM eventmanager
+
+INSERT INTO Musico VALUES('Rock', 'Guitarra', 5, 100.0, 1);
+
+INSERT INTO Musico VALUES('Rock', 'Viola', 5, 100.0, 1);
+
+INSERT INTO eventmanager VALUEs(10, 3)
