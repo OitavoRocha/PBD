61c6debde5fd13cb3efbff89dcf14c6376c8f680
João Vitor Farias <102336880+joaovfarias@users.noreply.github.com>
2024-02-27 19:29:17 -0300

    d

--- database.sql
+++ database.sql
@@ -2,7 +2,8 @@ CREATE TABLE musicfind.Usuario (
     idUsuario INT PRIMARY KEY,
     contato VARCHAR(11),
     rating FLOAT CHECK (rating <= 5.0 AND rating >= 0.0),
-    nome VARCHAR(30)
+    nome VARCHAR(30),
+    tipo VARCHAR(30) CHECK (tipo = 'Musico' OR tipo = 'Usuario')
 );
 
 CREATE TABLE musicfind.Musico (
@@ -13,18 +14,15 @@ CREATE TABLE musicfind.Musico (
     idUsuario INT PRIMARY KEY references musicfind.Usuario(idUsuario)
 );
 
-CREATE TABLE musicfind.EventManager (
-    anos_experiencia INT,
-    idUsuario INT PRIMARY KEY references musicfind.Usuario(idUsuario)
-);
-
 CREATE TABLE musicfind.Evento (
-    idEvento INT PRIMARY KEY,
     nome VARCHAR(30),
     capacidade INT,
-    data DATE,
+    foco VARCHAR(30),
     status_evento VARCHAR(30),
-    foco VARCHAR(30)
+    idEvento INT PRIMARY KEY,
+    data DATE,
+    endereço VARCHAR(30),
+    idUsuario INT references musicfind.Usuario(idUsuario)
 );
 
 CREATE TABLE musicfind.ParticipaMusico (
@@ -37,9 +35,9 @@ CREATE TABLE musicfind.Banda (
     idBanda INT NOT NULL,
     statusBanda VARCHAR(10),
     genero VARCHAR(10),
-    nome VARCHAR(50),
     cache FLOAT,
     rating FLOAT CHECK (rating >= 0 AND rating <= 5),
+    nome VARCHAR(50),
     PRIMARY KEY (idBanda)
 );
 
@@ -49,49 +47,19 @@ CREATE TABLE musicfind.ParticipaBanda (
     PRIMARY KEY (idEvento, idBanda)
 );
 
-CREATE TABLE musicfind.LocalEvento (
-    alvara VARCHAR(30),
-    lotacao_max INT,
-    endereco VARCHAR(30) PRIMARY KEY
-);
 
 CREATE TABLE musicfind.HistoricoMusico (
-    nomeEvento VARCHAR(30),                                         -- idEvento não seria melhor?
+    idEvento INT,                                         
     idMusico INT references musicfind.Musico(idUsuario),
     avaliacao FLOAT CHECK (avaliacao >= 0 AND avaliacao <= 5),
-    PRIMARY KEY (idMusico)                                          -- histórico musico não pode ter idmusico como chave primária
-);                                                                  -- pois um músico pode ter participado de vários eventos
-
-CREATE TABLE musicfind.Repertorio (
-    musica VARCHAR(30),
-    idMusico INT references musicfind.Musico(idUsuario),            -- idmusico como chave primária não faz sentido
-    PRIMARY KEY (idMusico)                                          -- pois um músico pode ter várias músicas no repertório
-);
-
-CREATE TABLE musicfind.GrandePorte (
-    idEvento INT references musicfind.Evento(idEvento),
-    PRIMARY KEY (idEvento)
-);
-
-CREATE TABLE musicfind.Em (
-    enderecoLocal varchar(30) references musicfind.LocalEvento(endereco),
-    idGrandePorte int references musicfind.GrandePorte(idEvento),
-    PRIMARY KEY (enderecoLocal, idGrandePorte)
-);
+    PRIMARY KEY (idEvento, idMusico)                                        
+);                                                                  
 
 CREATE TABLE musicfind.HistoricoBanda (
-    nomeEvento VARCHAR(50),
+    idEvento INT,
+    idBanda INT references musicfind.Banda(idBanda),
     avaliacao FLOAT CHECK (avaliacao >= 0 AND avaliacao <= 5),
-    idBanda INT,
-    PRIMARY KEY (idBanda),
-    FOREIGN KEY (idBanda) REFERENCES musicfind.Banda(idBanda)
-);
-
-CREATE TABLE musicfind.RepertorioBanda (
-    musica VARCHAR(50),
-    idBanda INT,
-    PRIMARY KEY (idBanda),
-    FOREIGN KEY (idBanda) REFERENCES musicfind.Banda(idBanda)
+    PRIMARY KEY (idEvento, idBanda)
 );
 
 CREATE TABLE musicfind.Afiliado (
@@ -100,19 +68,4 @@ CREATE TABLE musicfind.Afiliado (
     PRIMARY KEY (idMusico, idBanda),
     FOREIGN KEY (idMusico) REFERENCES musicfind.Usuario(idUsuario),
     FOREIGN KEY (idBanda) REFERENCES musicfind.Banda(idBanda)
-);
-
-CREATE TABLE musicfind.PequenoPorte (
-    endereco VARCHAR(50),
-    idEvento INT,
-    PRIMARY KEY (idEvento),
-    FOREIGN KEY (idEvento) REFERENCES musicfind.Evento(idEvento)
-);
-
-CREATE TABLE musicfind.Cria (
-    idEventManager INT,
-    idGrandePorte INT,
-    PRIMARY KEY (idEventManager, idGrandePorte),
-    FOREIGN KEY (idGrandePorte) REFERENCES musicfind.Evento(idEvento),
-    FOREIGN KEY (idEventManager) REFERENCES musicfind.Usuario(idUsuario)
 );
